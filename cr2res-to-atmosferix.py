1bee8bfb054de3884dcfdc27b811acc6b501ce4e
astro-alexis <alexis.lavail@physics.uu.se>
2023-10-16 11:38:57 +0200

    good improvements

--- cr2res-to-atmosferix.py
+++ cr2res-to-atmosferix.py
@@ -27,6 +27,9 @@ from scipy import interpolate
 from scipy.optimize import least_squares,minimize
 from astropy.time import Time
 from barycorrpy import utc_tdb, get_BC_vel
+import warnings
+import sys
+warnings.filterwarnings('ignore')
 
 def mincrosscol_wave(offset,w1,s1,wref,sref, eref):
     ii = ~np.isnan(eref)
@@ -54,13 +57,21 @@ def gen_corr_spec(w,wref,s,params):
     f = interpolate.interp1d(w,s, bounds_error=False, fill_value="extrapolate")
     s1 = f(wref)
     return s1
+
 # Read the filelist linking raw to reduced files
 filelist =  np.loadtxt('filelist-raw-to-reduced.ascii', skiprows=1, dtype="object")
 pair,rawf,nodpos = filelist[:,0],filelist[:,1],filelist[:,2]
 
+# Initialize the data dictionary
+data = {'nodpos' : np.empty(2048)}
+
 # Sorting raw file list by timestamp
 ii = np.argsort(rawf)
 pair,rawf,nodpos = pair[ii], rawf[ii], nodpos[ii]
+
+data['nodpos'] = nodpos
+data['rawfilename'] = rawf
+data['nodpair'] = pair
 
 # Creating a list of reduced files redf corresponding to the raw files
 redf, modelf, corrf = [], [], []
@@ -69,29 +80,24 @@ for i in range(len(rawf)):
     modelf.append(pair[i]+'/molecfit'+nodpos[i]+'/MODEL/BEST_FIT_MODEL.fits')
     corrf.append(pair[i]+'/molecfit'+nodpos[i]+'/CORRECT/SCIENCE_TELLURIC_CORR_SCIENCE.fits')
 
-### Namely: 
-# - orders: List of orders -- absolute nbs
-# - WW: Wavelength vector for each order [list obj (N_order,N_wav)]
-# - Ir: Intensity values for each order [list of 2D arrays (N_order,N_obs,N_wav)]
-# - Blaze: Blaze values for each order [list of 2D arrays (N_order,N_obs,N_wav)]
-# - Ia: Telluric spectra for each order [list of 2D arrays (N_order,N_obs,N_wav)]
-# - T_obs: Observation dates [BJD]
-# - phase: Planet orbital phase - centered on mid-transit
-# - window: Transit window (1 --> mid-transit; 0 --> out-of-transit)
-# - berv: BERV values [km/s]
-# - V0+Vp: Stellar RV [km/s]
-# - airmass:Airmass values
-# - SN: Signal-to-noise values for each order [N_order,N_obs]
+# Listing the orders which are present in both A and B spectra
+a1,b1 = np.squeeze(np.argwhere(nodpos=="A")[0]),np.squeeze(np.argwhere(nodpos=="B")[0])
+corrA,corrB = fits.open(corrf[a1]),fits.open(corrf[b1])
+oA = [corrA[i+1].header['EXTNAME'] for i in range(len(corrA)-1)]
+oB = [corrB[i+1].header['EXTNAME'] for i in range(len(corrB)-1)]
+ordernames = np.intersect1d(oA,oB)
+
 pp = np.arange(2008)+20
 
 mod = fits.open(modelf[0])[1].data
 
-nord = 18 # K2147 
+nord = len(ordernames) 
 nobs = len(rawf)
 npix = len(pp)
 
 w = np.zeros((nord,nobs,npix))
 I = np.zeros((nord,nobs,npix))
+E = np.zeros((nord,nobs,npix))
 tell = np.zeros((nord,nobs,npix))
 snr = np.zeros((nord,nobs))
 airmass=np.zeros((nobs))
@@ -105,6 +111,20 @@ window = np.zeros((nobs))
 for i in range(len(rawf)):
     # Open raw file
     raw = fits.open(rawf[i])
+    # Open reduced file
+    red = fits.open(redf[i])
+    hh = np.array(raw[0].header.cards)
+    fwhm1 = np.array([value for value in np.array(red['CHIP1.INT1'].header.cards) if "FWHM" in value[0]])
+    fwhm2 = np.array([value for value in np.array(red['CHIP2.INT1'].header.cards) if "FWHM" in value[0]])
+    fwhm3 = np.array([value for value in np.array(red['CHIP3.INT1'].header.cards) if "FWHM" in value[0]])
+    if i == 0:
+        hhs = hh.shape
+        data['rawheaders'] = np.empty([nobs,hhs[0],hhs[1]], dtype="object")
+        data['slitfunctionFWHM-det1'] = np.empty([nobs,fwhm1.shape[0],fwhm1.shape[1]], dtype="object")
+        data['slitfunctionFWHM-det2'] = np.empty([nobs,fwhm2.shape[0],fwhm2.shape[1]], dtype="object")
+        data['slitfunctionFWHM-det3'] = np.empty([nobs,fwhm3.shape[0],fwhm3.shape[1]], dtype="object")
+    data['rawheaders'][i,:,:] = hh
+    data['slitfunctionFWHM-det1'][i,:,:], data['slitfunctionFWHM-det2'][i,:,:],data['slitfunctionFWHM-det3'][i,:,:] = fwhm1, fwhm2, fwhm3
     # Open molecfit MODEL resuts
     mod = fits.open(modelf[i])[1].data
     cor = fits.open(corrf[i])
@@ -113,11 +133,12 @@ for i in range(len(rawf)):
     # loop over orders
     index = 0
     for o in range(len(cor)-1):
-        if cor[o+1].header['EXTNAME'] == 'det1_08_01':
+        if len(np.intersect1d(ordernames,cor[o+1].header['EXTNAME'])) ==0 :
             continue
         ii = np.squeeze(np.argwhere(mod['chip'] == o+1)) #not-telluric corrected case
-        w[index,i,:] = mod['lambda'][ii][pp] * 1000.
-        if nodpos[i] == 'B':
+        w[index,i,:] = mod['mlambda'][ii][pp] * 1000. # not-tell
+        if index > 0 :
+#        if nodpos[i] == 'B':
             # Cross-correlate mean up on mean down
             flux = mod['flux'][ii][pp] # not-telluric corrected spectrum
             res = least_squares(mincrosscol_scale, 1., args=(flux, I[index,0,:], flux*0.+1.), bounds=(0.,5.), verbose=0, max_nfev=2500)
@@ -135,6 +156,7 @@ for i in range(len(rawf)):
             tell[index,i,:] = mod['mflux'][ii][pp]
         w[index,i,:] = w[index,0,:]
         snr[index,i] = np.nanmedian(cor[index+1].data['SPEC'][pp]/cor[index+1].data['ERR'][pp])
+        E[index,i,:] = cor[index+1].data['ERR'][pp] 
         index+=1
 
     # Compute the UTC_TDB obs date
@@ -144,17 +166,32 @@ for i in range(len(rawf)):
     # Convert half_exptime from seconds to day
     jd = Time(2400000.5 + half_exptime + raw[0].header['MJD-OBS'], format='jd', scale='utc')
      # Convert JD_UTC to UTC_TDB
-    utc_tbd = utc_tdb.JDUTC_to_BJDTDB(jd, starname="GJ 3470",lat=raw[0].header['HIERARCH ESO TEL GEOLAT'],
+    utc_tbd = utc_tdb.JDUTC_to_BJDTDB(jd, starname=raw[0].header['OBJECT'],lat=raw[0].header['HIERARCH ESO TEL GEOLAT'],
                                 longi=raw[0].header['HIERARCH ESO TEL GEOLON'], alt=raw[0].header['HIERARCH ESO TEL GEOELEV'])
     Tobs[i] = np.squeeze(utc_tbd[0])
 
     # Compute BERV
-    bcvel = get_BC_vel(JDUTC=jd, starname="GJ 3470", lat=raw[0].header['HIERARCH ESO TEL GEOLAT'], 
+    bcvel = get_BC_vel(JDUTC=jd, starname=raw[0].header['OBJECT'], lat=raw[0].header['HIERARCH ESO TEL GEOLAT'], 
                                 longi=raw[0].header['HIERARCH ESO TEL GEOLON'], alt=raw[0].header['HIERARCH ESO TEL GEOELEV'], zmeas=0.0)
     berv[i] = np.squeeze(bcvel[0])
     # end of loop on files ##########################
 blaze=I
 
-savedata = (orders,w,I,blaze,tell,Tobs,phase,window,berv,V0Vp,airmass,snr)
-with open('gj3470-crires-test.pickle', 'wb') as specfile:
-    pickle.dump(savedata,specfile)
+wii = np.argsort(w[:,0,0])
+
+w = w[wii,:,:]
+I = I[wii,:,:]
+snr = snr[wii,:]
+
+data['orders'] = orders
+data['wavelength'] = w
+data['intensity'] = I
+data['tellurics'] = tell
+data['utc_tbd'] = Tobs
+data['berv'] = berv
+data['airmass'] = airmass
+data['snr'] = snr
+data['error'] = E
+#savedata = (orders,w,I,blaze,tell,Tobs,phase,window,berv,V0Vp,airmass,snr)
+with open(sys.argv[1], 'wb') as specfile:
+    pickle.dump(data,specfile)
