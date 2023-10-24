d847ac9f9cf3f231395705de0696fd3b30eb9a09
astro-alexis <alexis.lavail@physics.uu.se>
2023-10-24 14:26:17 +0200

    v1.0

--- cr2res-to-atmos.py
+++ cr2res-to-atmos.py
@@ -1,22 +1,11 @@
 # This scripts takes a reduced transit timeseries with CRIRES
 # reduced the usual way by Alexis and outputs the data in a 
-# pickle file, that can be loaded with the atmosferix pipeline
-# developed for SPIRou and MAROON-x data
+# pickle file,
 
-# For now, it is set to work for a precise case: data in K2148 setting
-# and more particularly the GJ 3470 GTO data
+# A documentation is available at : https://box.in2p3.fr/index.php/s/pCJKtJbzzD4NtaM
 
 # How you run:
-# python3 cr2res-to-atmosferix.py
-#
-
-# What you need:
-# - to be in the directory of the raw obs files CR*fits
-# - to have the usual 'filelist-raw-to-reduced.ascii' in the same dir linking raw to reduced files
-#   that file is output by nodding_mk-sofs.py
-# - to have the obs_nodding reduced files in `pairNN` directories with NN being a serie of two digit numbers
-# - to have molecfit outputs in `pairNN/molecfitA` and `pairNN/molecfitB` with products in `MODEL` `CORRECT` subdirs
-# - pray to the pagan gods that it works
+# python3 cr2res-to-atmos output.pickle
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -30,6 +19,8 @@ from barycorrpy import utc_tdb, get_BC_vel
 import warnings
 import sys
 warnings.filterwarnings('ignore')
+
+data = {'script_version' : '1.0'}
 
 def mincrosscol(params, w1,s1,wref,sref,eref):
     scale, offset = params[0], params[1]
@@ -51,13 +42,10 @@ def gen_corr_spec(w,wref,s,params):
     s1 = f(wref)
     return s1
 
-    
 # Read the filelist linking raw to reduced files
 filelist =  np.loadtxt('filelist-raw-to-reduced.ascii', skiprows=1, dtype="object")
 pair,rawf,nodpos = filelist[:,0],filelist[:,1],filelist[:,2]
 
-# Initialize the data dictionary
-data = {'nodpos' : np.empty(2048)}
 
 # Sorting raw file list by timestamp
 ii = np.argsort(rawf)
@@ -92,13 +80,14 @@ data['spec'] = np.zeros((nord,nobs,npix))
 data['err'] = np.zeros((nord,nobs,npix))
 data['wave_xcorr'] = np.zeros((nord,nobs,npix))
 data['spec_xcorr'] = np.zeros((nord,nobs,npix))
-
+data['spec_master'] = np.zeros((nord,npix))
 data['snr'] = np.zeros((nord,nobs))
 data['airmass'] = np.zeros((nobs))
 data['utc_tbd'] = np.zeros((nobs))
 data['berv'] = np.zeros((nobs))
 data['orders'] = np.arange((nord))+1
-
+data['fit_wave_offset'] = np.zeros((nord,nobs))
+data['fit_scaling'] = np.zeros((nord,nobs))
 masterA = fits.open('master/molecfitA/SCIENCE.fits')
 # MAIN LOOP
 for i in range(len(rawf)):
@@ -132,11 +121,13 @@ for i in range(len(rawf)):
         refflux = masterA[o+1].data['SPEC'][pp]
         # Cross-correlate mean up on mean down
         res = least_squares(mincrosscol, [1,0.], args=(wave, flux, waveM, refflux, error),bounds=([0.1,-0.1],[10,0.1]), x_scale=[0.1,0.0002],gtol=1e-12,ftol=1e-12, xtol=1e-12, max_nfev=10000)
+        data['fit_scaling'][index,i],data['fit_wave_offset'][index,i] = res.x[0], res.x[1]
         fluxinterp = gen_corr_spec(wave, waveM, flux ,[1.,res.x[1]])
         data['wave'][index,i,:], data['wave_xcorr'][index,i,:] = wave, waveM 
         data['spec'][index,i,:], data['spec_xcorr'][index,i,:] = flux, fluxinterp
         data['snr'][index,i] = np.nanmedian(flux/error)
         data['err'][index,i,:] = error
+        data['spec_master'][index,:] = refflux
         index+=1
 
     # Compute the UTC_TDB obs date
