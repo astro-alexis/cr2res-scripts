a205d72989cc781220585c0aa6ba214464f96a25
astro-alexis <alexis.lavail@physics.uu.se>
2023-10-27 11:10:22 +0200

    v2.0

--- cr2res-to-atmos.py
+++ cr2res-to-atmos.py
@@ -20,32 +20,11 @@ import warnings
 import sys
 warnings.filterwarnings('ignore')
 
-data = {'script_version' : '1.0'}
-
-def mincrosscol(params, w1,s1,wref,sref,eref):
-    scale, offset = params[0], params[1]
-    ii = ~np.isnan(eref)
-    w1,s1,wref,sref,eref = w1[ii],s1[ii],wref[ii],sref[ii],eref[ii]
-    ii = np.where(eref != 0)
-    w1,s1,wref,sref,eref = w1[ii],s1[ii],wref[ii],sref[ii],eref[ii]
-    w1n = offset + w1 
-    f1 = interpolate.interp1d(w1n, s1, bounds_error=False, fill_value="extrapolate")
-    s1i = f1(wref) * scale
-    return  (s1i[20:-20]-sref[20:-20])**2./eref[20:-20]**2
-
-def gen_corr_spec(w,wref,s,params):
-    scale,offset = params[0],params[1]
-    w1 = offset +  w
-    w = w1
-    s = s * scale
-    f = interpolate.interp1d(w,s, bounds_error=False, fill_value="extrapolate")
-    s1 = f(wref)
-    return s1
+data = {'script_version' : '2.0'}
 
 # Read the filelist linking raw to reduced files
 filelist =  np.loadtxt('filelist-raw-to-reduced.ascii', skiprows=1, dtype="object")
 pair,rawf,nodpos = filelist[:,0],filelist[:,1],filelist[:,2]
-
 
 # Sorting raw file list by timestamp
 ii = np.argsort(rawf)
@@ -55,19 +34,29 @@ data['nodpos'] = nodpos
 data['rawfilename'] = rawf
 data['nodpair'] = pair
 
-
 redf = [] # Initializing the array of reduced filenames
 
 # Loop on raw files, find the reduced files
 for i in range(len(rawf)):
-    redf.append(pair[i]+'/molecfit'+nodpos[i]+'/SCIENCE.fits')
+    redf.append(pair[i]+'/cr2res_obs_nodding_extracted'+nodpos[i]+'.normalized.fits')
+    
 
 # Open the first A and B:
 i0A, i0B = np.where(nodpos=='A')[0][0], np.where(nodpos=='B')[0][0]
 red0A, red0B = fits.open(redf[i0A]), fits.open(redf[i0B])
+
 # Listing the orders which are present in both A and B spectra
-oA = [red0A[i+1].header['EXTNAME'] for i in range(len(red0A)-1)]
-oB = [red0B[i+1].header['EXTNAME'] for i in range(len(red0B)-1)]
+roA1 = ["1-"+ord[0:2] for ord in red0A['CHIP1.INT1'].data.names if 'SPEC' in ord]
+roA2 = ["2-"+ord[0:2] for ord in red0A['CHIP2.INT1'].data.names if 'SPEC' in ord]
+roA3 = ["3-"+ord[0:2] for ord in red0A['CHIP3.INT1'].data.names if 'SPEC' in ord]
+oA = np.append(roA1,roA2)
+oA = np.append(oA,roA3)
+
+roB1 = ["1-"+ord[0:2] for ord in red0B['CHIP1.INT1'].data.names if 'SPEC' in ord]
+roB2 = ["2-"+ord[0:2] for ord in red0B['CHIP2.INT1'].data.names if 'SPEC' in ord]
+roB3 = ["3-"+ord[0:2] for ord in red0B['CHIP3.INT1'].data.names if 'SPEC' in ord]
+oB = np.append(roB1,roB2)
+oB = np.append(oB,roB3)
 ordernames = np.intersect1d(oA,oB)
 
 pp = np.arange(2008)+20
@@ -75,59 +64,70 @@ nord = len(ordernames)
 nobs = len(rawf)
 npix = len(pp)
 
+# Initializing most keys of the data dictionary
 data['wave'] = np.zeros((nord,nobs,npix))
+data['wave_model'] = np.zeros((nord,nobs,npix))
 data['spec'] = np.zeros((nord,nobs,npix))
 data['err'] = np.zeros((nord,nobs,npix))
-data['wave_xcorr'] = np.zeros((nord,nobs,npix))
-data['spec_xcorr'] = np.zeros((nord,nobs,npix))
-data['spec_master'] = np.zeros((nord,npix))
 data['snr'] = np.zeros((nord,nobs))
 data['airmass'] = np.zeros((nobs))
-data['utc_tbd'] = np.zeros((nobs))
+data['bjd_tdb'] = np.zeros((nobs))
 data['berv'] = np.zeros((nobs))
 data['orders'] = np.arange((nord))+1
-data['fit_wave_offset'] = np.zeros((nord,nobs))
-data['fit_scaling'] = np.zeros((nord,nobs))
-masterA = fits.open('master/molecfitA/SCIENCE.fits')
+
+
 # MAIN LOOP
 for i in range(len(rawf)):
     raw = fits.open(rawf[i]) # open raw file
     red = fits.open(redf[i]) # open reduce file
-
+    # Open molecfit model for MASTER A/B spectra
+    if nodpos[i] == 'A': master = fits.open('master/molecfitA/MODEL/BEST_FIT_MODEL.fits')[1].data
+    else: master = fits.open('master/molecfitB/MODEL/BEST_FIT_MODEL.fits')[1].data
     hh = np.array(raw[0].header.cards) # store raw header
     # Store FWHM of PSF from reduced file, for the 3 det
-    ##fwhm1 = np.array([value for value in np.array(red['CHIP1.INT1'].header.cards) if "FWHM" in value[0]])
-    ##fwhm2 = np.array([value for value in np.array(red['CHIP2.INT1'].header.cards) if "FWHM" in value[0]])
-    ##fwhm3 = np.array([value for value in np.array(red['CHIP3.INT1'].header.cards) if "FWHM" in value[0]])
+    fwhm1 = np.array([value for value in np.array(red['CHIP1.INT1'].header.cards) if "FWHM" in value[0]])
+    fwhm2 = np.array([value for value in np.array(red['CHIP2.INT1'].header.cards) if "FWHM" in value[0]])
+    fwhm3 = np.array([value for value in np.array(red['CHIP3.INT1'].header.cards) if "FWHM" in value[0]])
+
     if i == 0:
         hhs = hh.shape
         data['rawheaders'] = np.empty([nobs,hhs[0],hhs[1]], dtype="object")
-       ## data['slitfunctionFWHM-det1'] = np.empty([nobs,fwhm1.shape[0],fwhm1.shape[1]], dtype="object")
-       ## data['slitfunctionFWHM-det2'] = np.empty([nobs,fwhm2.shape[0],fwhm2.shape[1]], dtype="object")
-      ## data['slitfunctionFWHM-det3'] = np.empty([nobs,fwhm3.shape[0],fwhm3.shape[1]], dtype="object")
+        data['slitfunctionFWHM-det1'] = np.empty([nobs,fwhm1.shape[0],fwhm1.shape[1]], dtype="object")
+        data['slitfunctionFWHM-det2'] = np.empty([nobs,fwhm2.shape[0],fwhm2.shape[1]], dtype="object")
+        data['slitfunctionFWHM-det3'] = np.empty([nobs,fwhm3.shape[0],fwhm3.shape[1]], dtype="object")
     data['rawheaders'][i,:,:] = hh # store raw header in dictionary
-    ##data['slitfunctionFWHM-det1'][i,:,:], data['slitfunctionFWHM-det2'][i,:,:],data['slitfunctionFWHM-det3'][i,:,:] = fwhm1, fwhm2, fwhm3
+    data['slitfunctionFWHM-det1'][i,:,:], data['slitfunctionFWHM-det2'][i,:,:],data['slitfunctionFWHM-det3'][i,:,:] = fwhm1, fwhm2, fwhm3
+    # Compute airmass
     data['airmass'][i] = np.mean([raw[0].header['HIERARCH ESO TEL AIRM START'],raw[0].header['HIERARCH ESO TEL AIRM END']])
     print(rawf[i])
     # loop over orders
     index = 0
-    for o in range(len(red)-1):
-        if len(np.intersect1d(ordernames,red[o+1].header['EXTNAME'])) ==0 :
+    for o in ordernames:
+        # If the order exists in the file, load data. Else: go to next
+        try:
+            dd,oo = np.int32(o[0]), np.int32(o[2:])
+            wave = red[dd].data[str(oo).zfill(2)+'_01_WL'][pp]
+            flux = red[dd].data[str(oo).zfill(2)+'_01_SPEC'][pp]
+            error = red[dd].data[str(oo).zfill(2)+'_01_ERR'][pp]
+        except: 
             continue
-        wave = red[o+1].data['WAVE'][pp]
-        flux = red[o+1].data['SPEC'][pp]
-        error = red[o+1].data['ERR'][pp]
-        waveM = masterA[o+1].data['WAVE'][pp]
-        refflux = masterA[o+1].data['SPEC'][pp]
-        # Cross-correlate mean up on mean down
-        res = least_squares(mincrosscol, [1,0.], args=(wave, flux, waveM, refflux, error),bounds=([0.1,-0.1],[10,0.1]), x_scale=[0.1,0.0002],gtol=1e-12,ftol=1e-12, xtol=1e-12, max_nfev=10000)
-        data['fit_scaling'][index,i],data['fit_wave_offset'][index,i] = res.x[0], res.x[1]
-        fluxinterp = gen_corr_spec(wave, waveM, flux ,[1.,res.x[1]])
-        data['wave'][index,i,:], data['wave_xcorr'][index,i,:] = wave, waveM 
-        data['spec'][index,i,:], data['spec_xcorr'][index,i,:] = flux, fluxinterp
+        # If the wavelength is nan, order is fucked, go to next
+        if len(np.argwhere(np.isnan(wave))) > 0:
+            index+=1
+            continue
+        # Find the right order in master
+        for iim in range(len(np.unique(master['chip']))):
+            iimm = np.squeeze(np.argwhere(master['chip'] == iim+1))
+            wwm = master['mlambda'][iimm] * 1000
+            ffm = master['flux'][iimm]
+            if np.logical_and(np.nanmedian(wave) < np.max(wwm),np.nanmedian(wave) > np.min(wwm)):
+                waveM = wwm[pp]
+                refflux = ffm[pp]
+        data['wave'][index,i,:] = wave
+        data['wave_model'][index,i,:] = waveM
+        data['spec'][index,i,:] = flux
         data['snr'][index,i] = np.nanmedian(flux/error)
         data['err'][index,i,:] = error
-        data['spec_master'][index,:] = refflux
         index+=1
 
     # Compute the UTC_TDB obs date
@@ -139,7 +139,7 @@ for i in range(len(rawf)):
      # Convert JD_UTC to UTC_TDB
     utc_tbd = utc_tdb.JDUTC_to_BJDTDB(jd, starname=raw[0].header['OBJECT'],lat=raw[0].header['HIERARCH ESO TEL GEOLAT'],
                                 longi=raw[0].header['HIERARCH ESO TEL GEOLON'], alt=raw[0].header['HIERARCH ESO TEL GEOELEV'])
-    data['utc_tbd'] = np.squeeze(utc_tbd[0])
+    data['bjd_tdb'] = np.squeeze(utc_tbd[0])
 
     # Compute BERV
     bcvel = get_BC_vel(JDUTC=jd, starname=raw[0].header['OBJECT'], lat=raw[0].header['HIERARCH ESO TEL GEOLAT'], 
@@ -150,9 +150,8 @@ for i in range(len(rawf)):
 wii = np.argsort(data['wave'][:,0,0])
 
 data['wave'] = data['wave'][wii,:,:]
-data['wave_xcorr'] = data['wave_xcorr'][wii,:,:]
+data['wave_model'] = data['wave_model'][wii,:,:]
 data['spec'] = data['spec'][wii,:,:]
-data['spec_xcorr'] = data['spec_xcorr'][wii,:,:]
 data['err'] = data['err'][wii,:,:]
 data['snr'] = data['snr'][wii,:]
 
